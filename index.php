409da5885c0f94f51de91f859a0a415c5990dbbc
Kaspar Martin Suursalu <kasparsu@gmail.com>
2026-09-04 09:42:59 +0300

    Add Math examples

--- index.php
+++ index.php
@@ -1,13 +1,34 @@
 <?php
 $test = 10;
-$test = 3.1;
-$test = 'Hello';
-$test = 'õpilane';
-$test = '💩';
-$test = true;
-$test = false;
-$test = null;
-$test = [1, true, 'fsas'];
-$test = new stdClass();
-$test->name = 'Kaspar';
+$test = 10 + 10;
+$test = 10 - 10;
+$test = 10 * 10 * 10;
+$test = 10 / 10;
+$test = 5 % 2; 
+$test = 2 ** 3; // 2 * 2 * 2
+$test = $test + 2;
+$test += 2;
+$test -= 2;
+$test *= 2;
+$test /= 2;
+$test %= 2;
+$test **= 2;
+$test++;
+$test--;
+++$test;
+--$test;
+
+$a = 1;
+$b = ++$a;
+var_dump($b, $a);
+$test = 10;
+$test = sin($test);
 var_dump($test);
+/**
+ * 
+ * 
+ * asdas
+ * 
+ * 
+ * 
+ */
