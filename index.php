697e27bcf9efc388073f19e6603b23da8701940c
Kaspar Martin Suursalu <kasparsu@gmail.com>
2026-09-04 09:26:26 +0300

    Add types example

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
-<h1>
 <?php
-echo "Hello, World!";
-?>
-</h1>
+$test = 10;
+$test = 3.1;
+$test = 'Hello';
+$test = 'õpilane';
+$test = '💩';
+$test = true;
+$test = false;
+$test = null;
+$test = [1, true, 'fsas'];
+$test = new stdClass();
+$test->name = 'Kaspar';
+var_dump($test);
