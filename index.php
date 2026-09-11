537723757ce7a997d6167cec99703d6947d88cf0
Kaspar Martin Suursalu <kasparsu@gmail.com>
2026-09-11 10:02:10 +0300

    Add for and while loop examples

--- index.php
+++ index.php
@@ -1,52 +1,19 @@
 <?php
-$num = 10;
-if($num > 5) {
-    var_dump('Bigger');
-} else if ($num < 5) {
-    var_dump('Smaller');
-} else {
-    var_dump('Equal');
+
+for($i=0; $i<10; $i++){
+    var_dump($i);
 }
 
-$day = (int) date('w');
-var_dump($day);
-
-if($day === 0) {
-    var_dump('Sunday');
-} else if ($day === 1) {
-    var_dump('Monday');
-} else if ($day === 2) {
-    var_dump('Tuesday');
-} else if ($day === 3) {
-    var_dump('Wednesday');
-} else if ($day === 4) {
-    var_dump('Thursday');
-} else if ($day === 5 || $day === 6) {
-    var_dump('Partyday');
-} else {
-    var_dump('Weirdday');
+for($i=9; $i>=0; $i--){
+    var_dump($i);
+}
+for($i=1; $i<1_000_000; $i*=2){
+    var_dump($i);
 }
 
-switch($day) {
-    case 0:
-        var_dump('Sunday');
-        break;
-    case 1:
-        var_dump('Monday');
-        break;
-    case 2:
-        var_dump('Tuesday');
-        break;
-    case 3:
-        var_dump('Wednesday');
-        break;
-    case 4:
-        var_dump('Thursday');
-        break;
-    case 5:
-    case 6:
-        var_dump('Partyday'); 
-        break;
-    default:
-        var_dump('Weirdday');
+$time = time();
+$count = 0;
+while($time+1 > time()){
+    $count++;
 }
+var_dump($count);
