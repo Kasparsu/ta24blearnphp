bd931bd8264ef50c83868bfa5e9b57f1030efa1f
Kaspar Martin Suursalu <kasparsu@gmail.com>
2026-09-16 11:45:18 +0300

    Add visibility example

--- index.php
+++ index.php
@@ -1,12 +1,29 @@
 <?php
 
 class Box {
-    public $width;
-    public $height;
-    public $length;
+    public int $width;
+    private int $height;
+    protected int $length;
 
     public function volume(){
         return $this->width * $this->height * $this->length;
+    }
+
+    public function setHeight(int $height) {
+        if($height < 0) {
+            $this->height = 0;
+        } else {
+            $this->height = $height;
+        }
+    }
+
+    public function getHeight(){
+        return $this->height;
+    }
+
+    public function test1(){
+        $this->length = 10;
+        var_dump($this->length);
     }
 }
 
@@ -15,7 +32,17 @@ class MetalBox extends Box {
     public function mass(){
         return $this->volume() * $this->weightPerUnit;
     }
+
+    public function test2(){
+        $this->height = 10;
+        var_dump($this->height);
+    }
 }
 
+$box1 = new Box();
+$box1->test1();
+
+
 $metal1 = new MetalBox();
+$metal1->test2();
 var_dump($metal1);
