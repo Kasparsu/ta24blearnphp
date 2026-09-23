5baacf86e6c372577932753a353a34e2a5ec4f5a
Kaspar Martin Suursalu <kasparsu@gmail.com>
2026-09-23 11:17:33 +0300

    Add php templating example

--- index.php
+++ index.php
@@ -1,42 +1,24 @@
 <?php
-// library
+$name = 'Kaspar';
+?>
 
-class Job {
-    public function task(Logger $logger) {
-        for($i=0; $i<10; $i++) {
-            //we do some task (video editing, file processing etc)
-            
-            $logger->log("Task $i completed!");
-        }
-    }
-}
-
-class ConsoleLogger implements Logger {
-    public function log($message) {
-        echo "$message\n";
-    }
-}
-
-class NothingLogger implements Logger {
-    public function log($message) {
-
-    }
-}
-
-interface Logger {
-    public function log($message);
-}
-
-// user code
-
-class FileLogger implements Logger {
-    public function log($message) {
-        $file = fopen('log.txt', 'a');
-        fwrite($file, "$message\n");
-        fclose($file);
-    }
-}
-
-$job = new Job();
-$logger = new FileLogger();
-$job->task($logger);
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hello <?=$name?>!</h1>
+    <ul>
+        <?php for($i=0;$i<10;$i++): ?>
+            <?php if($i%2 === 0): ?>
+                <li style="color: green"><?=$i?></li>
+            <?php else: ?>
+                <li style="color: red"><?=$i?></li>
+            <?php endif ?>
+        <?php endfor ?>
+    </ul>
+</body>
+</html>
